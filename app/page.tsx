2bb93ad047dda51274f6f19128b39455aa7edf34
luisantonioj <luisantoniojopia@gmail.com>
2026-01-15 11:47:32 +0800

    feat(landing page): modify to a blank page

--- app/page.tsx
+++ app/page.tsx
@@ -13,13 +13,16 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-13">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Landing Page</Link>
+              <p>
+                OKS!
+              </p>
+              {/* <Link href={"/"}>Landing Page</Link>
               <div className="flex items-center gap-2">
                 <DeployButton />
-              </div>
+              </div> */}
             </div>
             {!hasEnvVars ? (
               <EnvVarWarning />
@@ -32,23 +35,11 @@ export default function Home() {
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           <Hero />
-          <main className="flex-1 flex flex-col gap-6 px-4">
-            <h2 className="font-medium text-xl mb-4">Next steps</h2>
-            {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-          </main>
         </div>
 
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-5">
           <p>
-            Powered by{" "}
-            <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Supabase
-            </a>
+            Operation Keep Safe (OKS!) Footer
           </p>
           <ThemeSwitcher />
         </footer>
